c3b5316a21fcdfc622112cad04f684e2385aaea8
michal <m.lejwoda@sunshineprofits.com>
2024-03-15 20:20:17 +0100

    Init

---
+++ my_flashcards/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from my_flashcards.users.models import User
+from my_flashcards.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
