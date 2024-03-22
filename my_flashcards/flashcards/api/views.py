476d8a0056a9a915659dd093b58e1ddb6f1fa3b7
michal <m.lejwoda@sunshineprofits.com>
2024-03-22 16:26:58 +0100

    Working tasks

--- my_flashcards/flashcards/api/views.py
+++ my_flashcards/flashcards/api/views.py
@@ -1,17 +1,15 @@
-import os
-
-from django.conf import settings
+from celery.result import AsyncResult
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
-from rest_framework.parsers import FileUploadParser, MultiPartParser
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ViewSet
 
 from my_flashcards.flashcards.api.serializers import DeckSerializer, SingleDeckSerializer
 from my_flashcards.flashcards.models import Deck
-from rest_framework.pagination import PageNumberPagination
-
 from my_flashcards.flashcards.tasks import get_words_from_file
 
 
@@ -47,19 +45,30 @@ class FileUploadViewSet(ViewSet):
         file_obj = request.FILES.get('file')
         if file_obj:
             try:
-                # Odczytanie zawartości pliku do ciągu bajtów
                 file_data = file_obj.read()
-                             # .read().decode('utf-8'))
-
-                # Przekazanie danych pliku jako argument do apply_async()
-                id = get_words_from_file.apply_async(args=[file_data]).id
-                print("test_id")
-                print(id)
-
-                return Response({"message": "Plik został pomyślnie przesłany"}, status=status.HTTP_200_OK)
+                task_id = get_words_from_file.apply_async(args=[file_data]).id
+                return Response({"status": "SUCCESS", "message": "Plik został pomyślnie przesłany", "task_id": task_id},
+                                status=status.HTTP_200_OK)
             except Exception as e:
-                print(e)
-                print(f"Błąd podczas odczytu pliku: {e}")
-                return Response({"error": "Błąd podczas odczytu pliku"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response({"status": "FAILED", "message": "Błąd podczas odczytu pliku"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:
-            return Response({"error": "Nie znaleziono pliku"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"status": "FAILED", "message": "Nie znaleziono pliku"}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
+    def get_task(self, request):
+        task_id = request.query_params.get('task_id')
+        if task_id:
+            try:
+                result = AsyncResult(task_id)
+                if result.successful():
+                    return Response({"status": "SUCCESS", "result": result.get()}, status=status.HTTP_200_OK)
+                elif result.failed():
+                    return Response({"status": "FAILED", "error": str(result.result)},
+                                    status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                else:
+                    return Response({"status": "PENDING", "message": "Task is still pending."},
+                                    status=status.HTTP_202_ACCEPTED)
+            except Exception as e:
+                return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response({"error": "Missing task_id parameter."}, status=status.HTTP_400_BAD_REQUEST)
