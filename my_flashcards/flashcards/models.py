df1e65f3f438c95a08f762a9e8b27c5e3cddcbe4
michal <m.lejwoda@sunshineprofits.com>
2024-03-23 19:33:34 +0100

    Model localization changes

--- my_flashcards/flashcards/models.py
+++ my_flashcards/flashcards/models.py
@@ -1,26 +1,32 @@
 from django.core.validators import MinLengthValidator
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
-
+from django.utils.translation import gettext_lazy as _
 from my_flashcards.users.models import User
 
 
 class WithVisitCounter(models.Model):
     popularity = models.IntegerField(editable=False, default=0)
+
     class Meta:
         abstract = True
 
+
 class Tag(WithVisitCounter, models.Model):
-    slug = models.SlugField(unique=True)
-    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, verbose_name=_('Slug'))
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
+
     def __str__(self):
         return "{}".format(self.name)
 
 
-
 class Word(TimeStampedModel):
-    front_side = models.CharField(max_length=200, validators=[MinLengthValidator(2, message='Pole musi zawierać co najmniej 2 znaki')])
-    back_side = models.CharField(max_length=200, validators=[MinLengthValidator(2, message='Pole musi zawierać co najmniej 2 znaki')])
+    front_side = models.CharField(max_length=200,
+                                  validators=[MinLengthValidator(2,message=_('The field must contain at least 2 characters'))],
+                                  verbose_name=_('Front side'))
+    back_side = models.CharField(max_length=200,
+                                 validators=[MinLengthValidator(2, message=_('The field must contain at least 2 characters'))],
+                                 verbose_name=_('Back side'))
     next_learn = models.DateTimeField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
 
@@ -32,19 +38,10 @@ class Word(TimeStampedModel):
 class Deck(WithVisitCounter, TimeStampedModel):
     name = models.CharField(max_length=100, blank=True)
     slug = models.SlugField(unique=True, blank=True)
-    words = models.ManyToManyField(Word, blank=True)
+    words = models.ManyToManyField(Word, blank=True, verbose_name=_('Words'))
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
-    is_public = models.BooleanField(default=False)
+    is_public = models.BooleanField(default=False, verbose_name=_('Is public'))
 
     def __str__(self):
         return "{} {}".format(self.name, self.user)
-
-class TempFileWords(TimeStampedModel):
-    front_side = models.CharField(max_length=200,
-                                  validators=[MinLengthValidator(2, message='Pole musi zawierać co najmniej 2 znaki')])
-    back_side = models.CharField(max_length=200,
-                                 validators=[MinLengthValidator(2, message='Pole musi zawierać co najmniej 2 znaki')])
-
-class TempFile(TimeStampedModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
-    temp_file_words = models.ManyToManyField(TempFileWords, blank=True)
