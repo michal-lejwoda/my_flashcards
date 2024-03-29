03ff2f887b74a4f373c49eb185b030f8ea38b204
michal <m.lejwoda@sunshineprofits.com>
2024-03-29 09:49:51 +0100

    test_create_word_for_deck

--- my_flashcards/flashcards/tests/test_views.py
+++ my_flashcards/flashcards/tests/test_views.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 from rest_framework.test import APIRequestFactory
 
@@ -9,16 +11,19 @@ from my_flashcards.users.models import User
 def test_check_number_of_words_in_deck(deck):
     assert deck.words.count() == 10
 
+
 class TestDeckViewSet:
     @pytest.fixture()
     def api_rf(self) -> APIRequestFactory:
         return APIRequestFactory()
+
     def test_get_queryset(self, user: User, deck: Deck, api_rf: APIRequestFactory):
         view = DeckViewSet()
         request = api_rf.get("/fake-url/")
         request.user = user
         view.request = request
         assert len(view.get_queryset()) == 1
+
 
 class TestSingleDeckViewSet:
     @pytest.fixture()
@@ -32,9 +37,19 @@ class TestSingleDeckViewSet:
         view.request = request
         assert len(view.get_queryset()) == 1
 
-    def test_delete_word_from_deck(self, user: User, words: Word, deck: Deck, api_rf: APIRequestFactory ):
+    def test_delete_word_from_deck(self, user: User, words: Word, deck: Deck, api_rf: APIRequestFactory):
         view = SingleDeckViewSet.as_view({'delete': 'delete_word_from_deck'})
         request = api_rf.delete("/fake-url/")
         request.user = user
         response = view(request, pk=deck.pk, word_id=words[0].id)
         assert response.status_code == 200
+
+    def test_create_word_for_deck(self, user: User, deck: Deck, api_rf: APIRequestFactory):
+        view = SingleDeckViewSet.as_view({'post': 'create_word_for_deck'})
+        request = api_rf.post("/fake-url/", data={
+        "front_side": "uprzejmy",
+        "back_side": "courteous"
+    })
+        request.user = user
+        response = view(request, pk=deck.pk)
+        assert response.status_code == 201
