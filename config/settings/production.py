4b8a16b74669a9e3c4c8854c3de4a18057304876
michal <michal.lejwoda@gmail.com>
2024-06-30 21:50:05 +0200

    add production changes

--- config/settings/production.py
+++ config/settings/production.py
@@ -19,8 +19,6 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
-print("ALLOWED_HOSTS")
-print(ALLOWED_HOSTS)
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
