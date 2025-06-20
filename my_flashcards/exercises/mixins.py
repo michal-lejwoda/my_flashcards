ae75fd8b66c232b158f8442f22f795e6a394ae07
michal <michal.lejwoda@gmail.com>
2025-06-20 16:46:56 +0200

    Add LayoutMixin

--- my_flashcards/exercises/mixins.py
+++ my_flashcards/exercises/mixins.py
@@ -1,5 +1,11 @@
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
 from wagtail.models import Page
 from slugify import slugify
+from django.db import models
+
+from my_flashcards.exercises.blocks import HeaderImageBlock, TextContentBlock, AudioContentBlock, TwoColumnBlock, \
+    SpacerBlock
 
 class AutoNumberedQuestionsMixin:
     def save(self, *args, **kwargs):
@@ -34,3 +40,28 @@ class UniqueSlugAcrossGroupPagesMixin:
             self.slug = self.generate_unique_slug(self.slug)
 
         super().clean()
+
+class LayoutMixin(models.Model):
+    before_layout_config = StreamField([
+        ('header_image', HeaderImageBlock()),
+        ('text_content', TextContentBlock()),
+        ('audio_content', AudioContentBlock()),
+        ('two_column', TwoColumnBlock()),
+        ('spacer', SpacerBlock()),
+    ], use_json_field=True, blank=True)
+
+    after_layout_config = StreamField([
+        ('header_image', HeaderImageBlock()),
+        ('text_content', TextContentBlock()),
+        ('audio_content', AudioContentBlock()),
+        ('two_column', TwoColumnBlock()),
+        ('spacer', SpacerBlock()),
+    ], use_json_field=True, blank=True)
+
+    layout_panels = [
+        FieldPanel('before_layout_config'),
+        FieldPanel('after_layout_config'),
+    ]
+
+    class Meta:
+        abstract = True
