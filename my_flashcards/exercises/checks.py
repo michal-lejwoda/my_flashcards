34ea12cafb49ff2326573ec24adb30e0378c955f
michal <michal.lejwoda@gmail.com>
2025-06-20 16:48:07 +0200

    check if instance

--- my_flashcards/exercises/checks.py
+++ my_flashcards/exercises/checks.py
@@ -1,13 +1,16 @@
+from wagtail.images.models import Image
+
 class MatchExercisesCheck:
     @staticmethod
     def check_pair_exercises(pairs):
-        print("pairs", pairs)
         correct_answers = []
         for block in pairs:
             if block.block_type != 'pair':
                 continue
             for pair in block.value:
                 left = pair.get('left_item')
+                if isinstance(left, Image):
+                    left = left.id
                 right = pair.get('right_item')
                 if left is None or right is None:
                     continue
