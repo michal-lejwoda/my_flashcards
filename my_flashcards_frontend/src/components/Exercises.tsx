37a41009a3c2d6543bafeee497e8db8c01179eb5
michal <michal.lejwoda@gmail.com>
2025-06-25 23:55:03 +0200

    Start working with groups

--- my_flashcards_frontend/src/components/Exercises.tsx
+++ my_flashcards_frontend/src/components/Exercises.tsx
@@ -1,18 +1,21 @@
 // import React from 'react';
 
 import {useLocation} from "react-router-dom";
+import {useEffect} from "react";
 
 const Exercises = () => {
     const location = useLocation();
     const fullPath = location.pathname; // np. "/exercises/test/abc"
     console.log("fullPath", fullPath)
-    const subPath = fullPath.replace(/^\/exercises\/?/, ''); // usuń "/exercises/"
-    const lastElement = subPath.split("/")
+    const subPath = fullPath.replace(/^\/exercises\/?/, '');
     console.log(subPath)
-    console.log("lastElement", lastElement[lastElement.length-1])
+    useEffect(()=>{
+        console.log("saddsasdsads", subPath)
+    },[])
     return (
         <div>
-            <h1>Ścieżka: {subPath}</h1>
+            <h1></h1>
+            {/*<h1>Ścieżka: {subPath}</h1>*/}
         </div>
     );
 };
