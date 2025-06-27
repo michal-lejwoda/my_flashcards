7f36e0b337f2d52601e2d176164fb04630dfe015
michal <michal.lejwoda@gmail.com>
2025-06-27 23:37:07 +0200

    Working LanguageGroup.tsx

--- my_flashcards_frontend/src/components/groupComponents/LanguageGroup.tsx
+++ my_flashcards_frontend/src/components/groupComponents/LanguageGroup.tsx
@@ -1,9 +1,14 @@
 import '../../sass/languagegroup.css'
 import {LanguageGroupProps} from "../../interfaces.tsx";
+import {useNavigate} from "react-router-dom";
 
 
 const LanguageGroup = ({ group }: LanguageGroupProps)  => {
     console.log("props.group", group)
+    const navigate = useNavigate();
+    const handleMoveToAnotherGroup = (path_slug: string) => {
+        navigate(`/exercises/${path_slug}`)
+    }
     return (
         <section className="languagegroup">
             <div className="languagegroup__image">
@@ -13,7 +18,7 @@ const LanguageGroup = ({ group }: LanguageGroupProps)  => {
                 <div className="languagegroup__title"><h1>LanguageGroup</h1></div>
                 <div className="languagegroup__languages">
                     {group.data.map(language => (
-                        <div className="languages__container" key={language.id}>
+                        <div onClick={()=>handleMoveToAnotherGroup(language.path_slug)} className="languages__container" key={language.id}>
                             <img src={"http://0.0.0.0:8000" + language.flag_image.url} alt=""/>
                             <div className="languages__title">
                                 {language.title}
