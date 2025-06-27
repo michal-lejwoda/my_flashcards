82f2d9beeb2bf56c69dd2c158d6c0bdac21d7d13
michal <michal.lejwoda@gmail.com>
2025-06-27 15:19:01 +0200

    Fix Components

--- my_flashcards_frontend/src/components/Exercise.tsx
+++ my_flashcards_frontend/src/components/Exercise.tsx
@@ -1,11 +1,8 @@
 import {useEffect, useState} from "react";
 import {handleGetExercise} from "../api.tsx";
 import {useParams} from "react-router-dom";
-import MainGroup from "./groupComponents/MainGroup.tsx";
-import LanguageGroup from "./groupComponents/LanguageGroup.tsx";
-import SubGroup from "./groupComponents/SubGroup.tsx";
-import GroupExercises from "./groupComponents/GroupExercises.tsx";
-import FillInTextExerciseWithChoicesWithImageDecoration from "./exerciseComponents/FillInTextExerciseWithChoicesWithImageDecoration.tsx";
+import FillInTextExerciseWithChoicesWithImageDecoration
+    from "./exerciseComponents/FillInTextExerciseWithChoicesWithImageDecoration.tsx";
 import FillInTextExerciseWithChoices from "./exerciseComponents/FillInTextExerciseWithChoices.tsx";
 import ChooseExerciseDependsOnMultipleTexts from "./exerciseComponents/ChooseExerciseDependsOnMultipleTexts.tsx";
 import ChooseExerciseDependsOnSingleText from "./exerciseComponents/ChooseExerciseDependsOnSingleText.tsx";
@@ -18,10 +15,15 @@ import MatchExercise from "./exerciseComponents/MatchExercise.tsx";
 import MatchExerciseTextWithImage from "./exerciseComponents/MatchExerciseTextWithImage.tsx";
 import MultipleExercises from "./exerciseComponents/MultipleExercises.tsx";
 
+interface ExerciseType {
+  type: string;
+}
+
 
 const Exercise = () => {
     const {id, slug} = useParams<{ id: string; slug: string }>();
-    const [exercise, setExercise] = useState(null)
+    const [exercise, setExercise] = useState<ExerciseType | null>(null);
+
     console.log("location", location)
     useEffect(() => {
         const fetchExercise = async () => {
@@ -34,36 +36,36 @@ const Exercise = () => {
     console.log("exercise", exercise)
     console.log(location)
     const renderContent = () => {
-    if (!exercise || !exercise.type) return null;
-    // #TODO BACK HERE
-    switch (exercise.type) {
-        case "MAIN_GROUP":
-            return <ChooseExerciseDependsOnMultipleTexts />;
-        case "LANGUAGE_GROUP":
-            return <ChooseExerciseDependsOnSingleText />;
-        case "SUB_GROUP":
-            return <ConjugationExercise />;
-        case "GROUP_EXERCISES":
-            return <FillInTextExerciseWithChoices />
-        case "SUB_GROUP":
-            return <FillInTextExerciseWithChoicesWithImageDecoration />;
-        case "GROUP_EXERCISES":
-            return <FillInTextExerciseWithPredefinedBlocks />
-        case "SUB_GROUP":
-            return <FlexibleExercisePage />;
-        case "GROUP_EXERCISES":
-            return <ListenWithManyOptionsToChooseToSingleExercise />
-        case "SUB_GROUP":
-            return <MatchExercise />;
-        case "GROUP_EXERCISES":
-            return <MatchExerciseTextWithImage />
-        case "GROUP_EXERCISES":
-            return <MultipleExercises />
+        if (!exercise || !exercise.type) return null;
+        // #TODO BACK HERE
+        switch (exercise.type) {
+            case "CHOOSE_EXERCISE_DEPENDS_ON_MULTIPLE_TEXTS":
+                return <ChooseExerciseDependsOnMultipleTexts/>;
+            case "CHOOSE_EXERCISE_DEPENDS_ON_SINGLE_TEXT":
+                return <ChooseExerciseDependsOnSingleText/>;
+            case "CONJUGATION_EXERCISE":
+                return <ConjugationExercise/>;
+            case "FILL_IN_TEXT_EXERCISE_WITH_CHOICES":
+                return <FillInTextExerciseWithChoices/>
+            case "FILL_IN_TEXT_EXERCISE_WITH_CHOICES_WITH_IMAGE_DECORATION":
+                return <FillInTextExerciseWithChoicesWithImageDecoration/>;
+            case "FILL_IN_TEXT_EXERCISE_WITH_PREDEFINED_BLOCKS":
+                return <FillInTextExerciseWithPredefinedBlocks/>
+            case "FLEXIBLE_EXERCISE_PAGE":
+                return <FlexibleExercisePage/>;
+            case "LISTEN_WITH_MANY_OPTIONS_TO_CHOOSE_TO_SINGLE_EXERCISE":
+                return <ListenWithManyOptionsToChooseToSingleExercise/>
+            case "MATCH_EXERCISE":
+                return <MatchExercise/>;
+            case "MATCH_EXERCISE_TEXT_WITH_IMAGE":
+                return <MatchExerciseTextWithImage/>
+            case "MULTIPLE_EXERCISES":
+                return <MultipleExercises/>
 
-        default:
-            return <h1>404</h1>;
-    }
-};
+            default:
+                return <h1>404</h1>;
+        }
+    };
     return (
         <div>
             {renderContent()}
