12d2c9c302bdd4bbef1a3fe8a1d6dfb8f6a1cdb5
michal <michal.lejwoda@gmail.com>
2025-08-29 13:47:30 +0200

    Fix navbar items visibility

--- my_flashcards_frontend/src/components/Navbar.tsx
+++ my_flashcards_frontend/src/components/Navbar.tsx
@@ -33,61 +33,32 @@ const Navbar = () => {
     };
 
     return (
-        <nav>
-            <div className="navbar__desktop desktop">
-                <ul className="desktop__menu menu">
-                    <NavLink to="/" className="navbar__item">
-                        <li className="menu__element">{t("decks")}</li>
-                    </NavLink>
-                    <NavLink to="/search" className="navbar__item">
-                        <li className="menu__element">{t("search")}</li>
-                    </NavLink>
-                    <NavLink to="/create" className="navbar__item">
-                        <li className="menu__element">{t("create")}</li>
-                    </NavLink>
-                    <NavLink to="/add_file" className="navbar__item">
-                        <li className="menu__element">{t("add_file")}</li>
-                    </NavLink>
-                    <NavLink to="/exercises" className="navbar__item">
-                        <li className="menu__element">{t("exercises")}</li>
-                    </NavLink>
-                </ul>
-                <ul className="desktop__menu">
-                    <select className="menu__language--select navbar__select" value={language}
-                            onChange={onChangeLanguage}>
-                        {LANGUAGES.map(({code, label}) => (
-                            <option key={code} value={code}>
-                                {label}
-                            </option>
-                        ))}
-                    </select>
+    <nav>
+        <div className="navbar__desktop desktop">
+            <ul className="desktop__menu menu">
+                {token && (
+                    <>
+                        <NavLink to="/" className="navbar__item">
+                            <li className="menu__element">{t("decks")}</li>
+                        </NavLink>
+                        <NavLink to="/search" className="navbar__item">
+                            <li className="menu__element">{t("search")}</li>
+                        </NavLink>
+                        <NavLink to="/create" className="navbar__item">
+                            <li className="menu__element">{t("create")}</li>
+                        </NavLink>
+                        <NavLink to="/add_file" className="navbar__item">
+                            <li className="menu__element">{t("add_file")}</li>
+                        </NavLink>
+                    </>
+                )}
+                <NavLink to="/exercises" className="navbar__item">
+                    <li className="menu__element">{t("exercises")}</li>
+                </NavLink>
+            </ul>
 
-                    {token &&
-                        <>
-                            <NavLink to="/account" className="navbar__item">
-                                <li className="menu__element ">{t("account")}</li>
-                            </NavLink>
-                            <div className="navbar__item" onClick={handleLogout}>
-                                <li className="menu__element">{t("logout")}</li>
-                            </div>
-                        </>
-                    }
-                    {!token &&
-                        <>
-                            <NavLink to="/login" className="navbar__item">
-                                <li className="menu__element">{t("login")}</li>
-                            </NavLink>
-                            <NavLink to="/register" className="navbar__item">
-                                <li className="menu__element">{t("register")}</li>
-                            </NavLink>
-                        </>
-                    }
-                </ul>
-            </div>
-            <div className="navbar__mobile mobile">
-                <div className="mobile__toggle" onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}><FontAwesomeIcon
-                    size="2x" icon={faBars}/></div>
-                <select className="mobile__language--select navbar__select" value={language}
+            <ul className="desktop__menu">
+                <select className="menu__language--select navbar__select" value={language}
                         onChange={onChangeLanguage}>
                     {LANGUAGES.map(({code, label}) => (
                         <option key={code} value={code}>
@@ -95,48 +66,83 @@ const Navbar = () => {
                         </option>
                     ))}
                 </select>
-                <ul className="mobile__menu" style={{display: isMobileNavOpen ? 'flex' : 'none'}}>
-                    <NavLink className="navbar__item" onClick={() => setIsMobileNavOpen(false)} to="/">
-                        <li className="mobile__menu__element ">{t("decks")}</li>
-                    </NavLink>
-                    <NavLink className="navbar__item" onClick={() => setIsMobileNavOpen(false)} to="/search">
-                        <li className="mobile__menu__element">{t("search")}</li>
-                    </NavLink>
-                    <NavLink className="navbar__item" onClick={() => setIsMobileNavOpen(false)} to="/create">
-                        <li className="mobile__menu__element">{t("create")}</li>
-                    </NavLink>
-                    <NavLink className="navbar__item" onClick={() => setIsMobileNavOpen(false)} to="/add_file">
-                        <li className="mobile__menu__element">{t("add_file")}</li>
-                    </NavLink>
-                    <NavLink className="navbar__item" onClick={() => setIsMobileNavOpen(false)} to="/exercises">
-                        <li className="mobile__menu__element">{t("exercises")}</li>
-                    </NavLink>
-                    {token &&
-                        <>
-                            <NavLink to="/account" className="navbar__item" onClick={() => setIsMobileNavOpen(false)}>
-                                <li className="mobile__menu__element">{t("account")}</li>
-                            </NavLink>
-                            <div className="navbar__item" onClick={closeMenuAndLogout}>
-                                <li className="mobile__menu__element">{t("logout")}</li>
-                            </div>
-                        </>
-                    }
-                    {!token &&
-                        <>
-                            <NavLink to="/login" className="navbar__item" onClick={() => setIsMobileNavOpen(false)}>
-                                <li className="mobile__menu__element">{t("login")}</li>
-                            </NavLink>
-                            <NavLink to="/register" className="navbar__item" onClick={() => setIsMobileNavOpen(false)}>
-                                <li className="mobile__menu__element">{t("register")}</li>
-                            </NavLink>
-                        </>
-                    }
-                </ul>
 
+                {token ? (
+                    <>
+                        <NavLink to="/account" className="navbar__item">
+                            <li className="menu__element">{t("account")}</li>
+                        </NavLink>
+                        <div className="navbar__item" onClick={handleLogout}>
+                            <li className="menu__element">{t("logout")}</li>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <NavLink to="/login" className="navbar__item">
+                            <li className="menu__element">{t("login")}</li>
+                        </NavLink>
+                        <NavLink to="/register" className="navbar__item">
+                            <li className="menu__element">{t("register")}</li>
+                        </NavLink>
+                    </>
+                )}
+            </ul>
+        </div>
 
+        <div className="navbar__mobile mobile">
+            <div className="mobile__toggle" onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}>
+                <FontAwesomeIcon size="2x" icon={faBars}/>
             </div>
-        </nav>
-    );
-};
+            <select className="mobile__language--select navbar__select" value={language}
+                    onChange={onChangeLanguage}>
+                {LANGUAGES.map(({code, label}) => (
+                    <option key={code} value={code}>
+                        {label}
+                    </option>
+                ))}
+            </select>
+            <ul className="mobile__menu" style={{display: isMobileNavOpen ? 'flex' : 'none'}}>
+                {token && (
+                    <>
+                <NavLink className="navbar__item" onClick={() => setIsMobileNavOpen(false)} to="/">
+                    <li className="mobile__menu__element ">{t("decks")}</li>
+                </NavLink>
+                <NavLink className="navbar__item" onClick={() => setIsMobileNavOpen(false)} to="/search">
+                    <li className="mobile__menu__element">{t("search")}</li>
+                </NavLink>
+                <NavLink className="navbar__item" onClick={() => setIsMobileNavOpen(false)} to="/create">
+                    <li className="mobile__menu__element">{t("create")}</li>
+                </NavLink>
+                <NavLink className="navbar__item" onClick={() => setIsMobileNavOpen(false)} to="/add_file">
+                    <li className="mobile__menu__element">{t("add_file")}</li>
+                </NavLink>
+                   </>
+            )}
+                <NavLink className="navbar__item" onClick={() => setIsMobileNavOpen(false)} to="/exercises">
+                    <li className="mobile__menu__element">{t("exercises")}</li>
+                </NavLink>
 
-export default Navbar;
+                {token ? (
+                    <>
+                        <NavLink to="/account" className="navbar__item" onClick={() => setIsMobileNavOpen(false)}>
+                            <li className="mobile__menu__element">{t("account")}</li>
+                        </NavLink>
+                        <div className="navbar__item" onClick={closeMenuAndLogout}>
+                            <li className="mobile__menu__element">{t("logout")}</li>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <NavLink to="/login" className="navbar__item" onClick={() => setIsMobileNavOpen(false)}>
+                            <li className="mobile__menu__element">{t("login")}</li>
+                        </NavLink>
+                        <NavLink to="/register" className="navbar__item" onClick={() => setIsMobileNavOpen(false)}>
+                            <li className="mobile__menu__element">{t("register")}</li>
+                        </NavLink>
+                    </>
+                )}
+            </ul>
+        </div>
+    </nav>
+);}
+export default Navbar
